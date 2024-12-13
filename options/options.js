7416a90161c27280e62be7087446689ed3652d2f
Crowdbotics <team@crowdbotics.com>
2024-12-13 17:56:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "swiftauth-50351",
+  url: "https://swiftauth-50351.botics.co",
+  api: "https://swiftauth-50351.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
